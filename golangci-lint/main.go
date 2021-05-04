a42d74a973570d2d4e7aeacaad9c96875a9b4b92
peperoncino <2wua4nlyi@gmail.com>
2021-05-05 04:10:03 +0900

    for lint error

--- golangci-lint/main.go
+++ golangci-lint/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"strconv"
 )
 
 func main() {
-	i, err := strconv.Atoi("255")
-	if err != nil {
-		os.Exit(1)
-	}
-
-	fmt.Println(i)
+	strconv.Atoi("255")
 }
